c3d05965bc463e4a1b214e9b774f578ff92c76f8
Christyan Shindy <christyan@sattvaconsultoria.com.br>
2025-06-10 18:03:02 -0300

    inserido componentes pagina login

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,35 @@
-import '../style.css'
+import { createGlobalStyle, ThemeProvider } from "styled-components"
 
+import theme from "../src/theme"
+
+const GlobalStyle = createGlobalStyle `
+* {
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-family: 'Roboto', sans-serif;
+  color: ${props => props.theme.black};
+}
+
+a {
+  color:${props => props.theme.primary};
+  font-weight: bold;
+  text-decoration: none;
+  transition: all 0.3s;
+}
+
+a:hover {
+  color: ${props => props.theme.primaryHover};
+}
+`
 function App ({ Component, pageProps }) {
   return (
+    <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
---
+++ pages/login.jsx
@@ -0,0 +1,40 @@
+import ImageWithSpace from "../src/components/layout/ImageWithSpace"
+import styled from "styled-components"
+import H1 from "../src/components/typography/H1"
+import H2 from "../src/components/typography/H2"
+import H4 from "../src/components/typography/H4"
+import Button from "../src/components/inputs/Button"
+import Input from "../src/components/inputs/Input"
+
+const FormContainer = styled.div`
+margin-top: 60px
+`
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 20px 0;
+  gap: 20px;
+`
+
+const Text = styled.p`
+  text-align: center;
+`
+function LoginPage() {
+  return(
+    <ImageWithSpace>
+      <H1># Social DEV</H1>
+      <H4>Tudo que acontece no mundo DEV, acontece aqui!</H4>
+      <FormContainer>
+        <H2>Entre em sua conta</H2>
+        <Form>
+          <Input label="Email ou usuário" type="email"/>
+          <Input label="Senha" type="password"/>
+          <Button>Entrar</Button>
+          <Text>Não possui uma conta? <a href="#">Faça seu cadastro</a></Text>
+        </Form>
+      </FormContainer>
+    </ImageWithSpace>
+  )
+}
+
+export default LoginPage
