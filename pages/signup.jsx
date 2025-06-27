a5f00ddd967361c0ede5a86a0308e4b8883ee689
Christyan Shindy <christyan@sattvaconsultoria.com.br>
2025-06-26 21:41:52 -0300

    adicionado react hook form no signup

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,12 +1,15 @@
 import ImageWithSpace from "../src/components/layout/ImageWithSpace"
 import styled from "styled-components"
 import Link from "next/link"
+import { useForm } from "react-hook-form"
+import { joiResolver } from "@hookform/resolvers/joi"
 import H1 from "../src/components/typography/H1"
 import H2 from "../src/components/typography/H2"
 import H4 from "../src/components/typography/H4"
 import Button from "../src/components/inputs/Button"
 import Input from "../src/components/inputs/Input"
 import { useState } from "react"
+import { signupSchema } from "../modules/user/user.schema"
 
 const FormContainer = styled.div`
 margin-top: 60px
@@ -22,35 +25,28 @@ const Text = styled.p`
   text-align: center;
 `
 function SignupPage() {
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setlastName] = useState('')
-  const [user, setUser] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  
-  const handleForm = (event) => {
-    event.preventDefault()
-    console.log({
-      firstName,
-      lastName,
-      user,
-      email,
-      password
-    })
+  const { register, handleSubmit, formState: { errors } } = useForm({ 
+    resolver: joiResolver(signupSchema)
+  })
+  const handleForm = (data) => {
+    console.log(data)
   }
+
+    console.log(errors)
+
   return(
     <ImageWithSpace>
       <H1># Social DEV</H1>
       <H4>Tudo que acontece no mundo DEV, acontece aqui!</H4>
       <FormContainer>
         <H2>Crie sua conta</H2>
-        <Form onSubmit={handleForm}>
-          <Input label="Nome" onChange={({ target }) => {setFirstName(target.value)}}/>
-          <Input label="Sobrenome" onChange={({ target }) => {setlastName(target.value)}}/>
-          <Input label="Usuário" onChange={({ target }) => {setUser(target.value)}}/>
-          <Input label="Email" type="email" onChange={({ target }) => {setEmail(target.value)}}/>
-          <Input label="Senha" type="password" onChange={({ target }) => {setPassword(target.value)}}/>
-          <Button>Cadastrar</Button>
+        <Form onSubmit={handleSubmit(handleForm)}>
+          <Input label="Nome" {...register('firstName')}/>
+          <Input label="Sobrenome" {...register('lastName')}/>
+          <Input label="Usuário" {...register('user')}/>
+          <Input label="Email" type="email" {...register('email')}/>
+          <Input label="Senha" type="password" {...register('password')}/>
+          <Button type="submit">Cadastrar</Button>
           <Text>Já possui uma conta? <Link href="/login">Faça seu login</Link></Text>
         </Form>
       </FormContainer>
